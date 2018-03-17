86e2e4aca9d0c5fdc8e6dd0f46979a5218d0260d
cborgtech <guru@cborgtech.com>
2018-03-17 16:12:42 -0400

    minor change during testing menu updates

--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -1,37 +1,46 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import Navigation from '../../coreWeb/navigation/NavigationView.js';
-import callService from '../api/api';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Navigation from "../../coreWeb/navigation/NavigationView.js";
+import callService from "../api/api";
+import { connect } from "react-redux";
 
 class NavigationContainer extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
+  }
 
-	}
-
-	render() {
-		let menus = {};
-		if (this.props.menus != null) {
-			menus = this.props.menus;
-		}
-		return (
-			<Navigation appPrefs={this.props.appPrefs} menus={menus} menuName={this.props.menuName} navClick={this.props.navClick}/>
-		);
-	}
+  render() {
+    let menus = {};
+    if (this.props.menus != null) {
+      menus = this.props.menus;
+    }
+    return (
+      <Navigation
+        appPrefs={this.props.appPrefs}
+        menus={menus}
+        menuName={this.props.menuName}
+        navClick={this.props.navClick}
+      />
+    );
+  }
 }
 
 NavigationContainer.propTypes = {
-	appPrefs: PropTypes.object.isRequired,
-	menuName: PropTypes.string.isRequired,
-	navClick: PropTypes.func.isRequired,
-	menus: PropTypes.object,
-	lang: PropTypes.string,
-	appGlobal: PropTypes.object
+  appPrefs: PropTypes.object.isRequired,
+  menuName: PropTypes.string.isRequired,
+  navClick: PropTypes.func.isRequired,
+  menus: PropTypes.object,
+  lang: PropTypes.string,
+  appGlobal: PropTypes.object
 };
 
 function mapStateToProps(state, ownProps) {
-  return {menus:state.appMenus, lang:state.lang, appPrefs:state.appPrefs, appGlobal:state.appPrefs.appGlobal};
+  return {
+    menus: state.appMenus,
+    lang: state.lang,
+    appPrefs: state.appPrefs,
+    appGlobal: state.appPrefs.appGlobal
+  };
 }
 
 export default connect(mapStateToProps)(NavigationContainer);
