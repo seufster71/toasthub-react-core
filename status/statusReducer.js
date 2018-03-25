ca7e546b1801a04e18261ceae8d48601b38096c5
seufster <seufster@gmail.com>
2018-03-25 19:28:07 -0400

    member menus

--- status/statusReducer.js
+++ status/statusReducer.js
@@ -11,10 +11,42 @@ export default function statusReducer(state = {}, action) {
       let myState = {};
       if(action.error != null) {
         myState.error = action.error;
+        myState.info = null;
+        myState.warn = null;
       }
+      return Object.assign({}, state, myState);
+    }
+    case 'SAVE_AUTHENTICATION': {
+      let myState = updateStatus(action);
       return Object.assign({}, state, myState);
     }
     default:
       return state;
   }
 }
+
+const updateStatus = ( action) => {
+  let myState = {};
+  if (action.responseJson != null && action.responseJson.params != null && action.responseJson.params.status != null) {
+    if (action.responseJson.params.status.info != null) {
+      myState.info = action.responseJson.params.status.info;
+    } else {
+      myState.info = null;
+    }
+    if (action.responseJson.params.status.warn != null) {
+      myState.warn = action.responseJson.params.status.warn;
+    } else {
+      myState.warn = null;
+    }
+    if (action.responseJson.params.status.error != null) {
+      myState.error = action.responseJson.params.status.error;
+    } else {
+      myState.error = null;
+    }
+  } else {
+    myState.error = null;
+    myState.info = null;
+    myState.warn = null;
+  }
+  return myState;
+};
