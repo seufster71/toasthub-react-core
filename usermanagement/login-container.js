3ef7388d3f7965bfc7c8c2d9c67030844f7b47a8
seufster <seufster@gmail.com>
2018-05-07 08:36:36 -0400

    dir cleanup

--- usermanagement/login-container.js
+++ usermanagement/login-container.js
@@ -5,7 +5,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import callService from '../api/api-call';
-import LoginView from '../../coreView/usermanagement/login-view';
+import LoginView from '../../coreView/userManagement/login-view';
 import utils from '../common/utils';
 import {connect} from 'react-redux';
 import * as userManagementActions from './usermanagement-actions';
