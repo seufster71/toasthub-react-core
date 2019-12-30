3492fc6a8e12b1eb597efd4830808d0599e9319f
Edward Seufert <edwardseufert@Edwards-MBP.home>
2019-12-29 21:06:45 -0500

    Update admin users

--- common/action-utils.js
+++ common/action-utils.js
@@ -4,7 +4,7 @@ const checkConnectivity = (responseJson,dispatch) => {
 	if (responseJson != null && responseJson.protocalError != null && responseJson.protocalError == 401){
 		dispatch({ type: "PROCESS_LOGOUT" });
 	} else {
-		dispatch({type:'SHOW_STATUS_ERROR',error:["Connectivity issue"]});
+		dispatch({type:'SHOW_STATUS_ERROR',error:[{code:"CONNECTIVITYFAILED",message:"Connectivity issue"}]});
 	}
 };
 
