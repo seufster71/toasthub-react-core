75eae9ba18c4f586c030c3ca8c67821adc053cfb
seufster71 <seufster@gmail.com>
2022-09-23 08:12:49 -0400

    refactor member stuff

---
+++ session/session-actions.js
@@ -0,0 +1,32 @@
+import callService from '../api/api-call';
+import actionUtils from '../common/action-utils';
+
+// actions
+
+// thunk
+export function sessionCheck() {
+	return function(dispatch) {
+		let requestParams = {};
+		requestParams.action = "CHECK";
+		requestParams.service = "MEMBER_SVC";
+		let params = {};
+		params.requestParams = requestParams;
+		params.URI = '/api/member/callService';
+
+		return callService(params).then( (responseJson) => {
+			if (responseJson != null && responseJson.protocalError == null){
+				dispatch({ type: "LOAD_SESSION_CHECK", responseJson });
+			} else {
+				actionUtils.checkConnectivity(responseJson,dispatch);
+			}
+		}).catch(error => {
+			throw(error);
+		});
+	};
+}
+
+export function viewPortChange(width, height) {
+	return function(dispatch) {
+		dispatch({ type: "VIEW_PORT_CHANGE", width, height });
+	};
+}
